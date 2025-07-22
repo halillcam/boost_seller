debb2e49172100afd780dfef92720afaf922c8d1
Halil Ibrahim Cam <halil.cam0020@gmail.com>
2025-07-22 20:01:08 +0300

    Mock data procceses Completed

---
+++ lib/model/datas.dart
@@ -0,0 +1,138 @@
+import 'package:e_commerce/model/data_model.dart';
+import 'package:e_commerce/model/items_constants.dart';
+
+class Datas {
+  static List<DataModel> getItems() {
+    List<DataModel> items = [];
+    items.add(
+      DataModel(
+        itemName: ItemsTexts.gucci_canta,
+        itemCategory: ItemCategories.Fashion.name,
+        itemPrice: ItemPrices.gucci_price,
+        itemImage: ImageUrls.gucciCanta,
+        itemSourceSite: [
+          itemSourceSite.Babazon.name,
+          itemSourceSite.Tabu.name,
+          itemSourceSite.Trendway.name,
+        ],
+        suppliers: [
+          ItemSuppliers.aliFather.name,
+          ItemSuppliers.babey.name,
+          ItemSuppliers.dropper.name,
+        ],
+      ),
+    );
+
+    items.add(
+      DataModel(
+        itemName: ItemsTexts.galatasaray_ceket,
+        itemCategory: ItemCategories.Fashion.name,
+        itemPrice: ItemPrices.ceket_price,
+        itemImage: ImageUrls.gsCeket,
+        itemSourceSite: [
+          itemSourceSite.Babazon.name,
+          itemSourceSite.Tabu.name,
+          itemSourceSite.Trendway.name,
+        ],
+        suppliers: [
+          ItemSuppliers.aliFather.name,
+          ItemSuppliers.babey.name,
+          ItemSuppliers.dropper.name,
+        ],
+      ),
+    );
+    items.add(
+      DataModel(
+        itemName: ItemsTexts.iphone_telefon,
+        itemCategory: ItemCategories.Electronics.name,
+        itemPrice: ItemPrices.iphone_price,
+        itemImage: ImageUrls.iphoneTelefon,
+        itemSourceSite: [
+          itemSourceSite.Babazon.name,
+          itemSourceSite.Tabu.name,
+          itemSourceSite.Trendway.name,
+          itemSourceSite.xBay.name,
+        ],
+        suppliers: [
+          ItemSuppliers.aliFather.name,
+          ItemSuppliers.babey.name,
+          ItemSuppliers.dropper.name,
+        ],
+      ),
+    );
+    items.add(
+      DataModel(
+        itemName: ItemsTexts.kazak,
+        itemCategory: ItemCategories.Fashion.name,
+        itemPrice: ItemPrices.kazak_price,
+        itemImage: ImageUrls.kazak,
+        itemSourceSite: [
+          itemSourceSite.Babazon.name,
+          itemSourceSite.Tabu.name,
+          itemSourceSite.Trendway.name,
+        ],
+        suppliers: [
+          ItemSuppliers.aliFather.name,
+          ItemSuppliers.babey.name,
+          ItemSuppliers.dropper.name,
+        ],
+      ),
+    );
+    items.add(
+      DataModel(
+        itemName: ItemsTexts.macbook_pro,
+        itemCategory: ItemCategories.Electronics.name,
+        itemPrice: ItemPrices.macbook_price,
+        itemImage: ImageUrls.mackbookPro,
+        itemSourceSite: [
+          itemSourceSite.Babazon.name,
+          itemSourceSite.Tabu.name,
+          itemSourceSite.Trendway.name,
+          itemSourceSite.xBay.name,
+        ],
+        suppliers: [
+          ItemSuppliers.aliFather.name,
+          ItemSuppliers.babey.name,
+          ItemSuppliers.dropper.name,
+        ],
+      ),
+    );
+    items.add(
+      DataModel(
+        itemName: ItemsTexts.rolex_saat,
+        itemCategory: ItemCategories.Electronics.name,
+        itemPrice: ItemPrices.rolex_price,
+        itemImage: ImageUrls.rolexSsaat,
+        itemSourceSite: [
+          itemSourceSite.xBay.name,
+          itemSourceSite.Tabu.name,
+          itemSourceSite.Trendway.name,
+        ],
+        suppliers: [
+          ItemSuppliers.aliFather.name,
+          ItemSuppliers.babey.name,
+          ItemSuppliers.dropper.name,
+        ],
+      ),
+    );
+    items.add(
+      DataModel(
+        itemName: ItemsTexts.samsung_telefon,
+        itemCategory: ItemCategories.Electronics.name,
+        itemPrice: ItemPrices.samsung_price,
+        itemImage: ImageUrls.samsungTelefon,
+        itemSourceSite: [
+          itemSourceSite.Babazon.name,
+          itemSourceSite.Tabu.name,
+          itemSourceSite.Trendway.name,
+        ],
+        suppliers: [
+          ItemSuppliers.aliFather.name,
+          ItemSuppliers.babey.name,
+          ItemSuppliers.dropper.name,
+        ],
+      ),
+    );
+    return items;
+  }
+}
---
+++ lib/model/items_constants.dart
@@ -0,0 +1,38 @@
+class ItemsTexts {
+  static const String gucci_canta = "Gucci Canta";
+  static const String iphone_telefon = "Iphone 15";
+  static const String sweet_shirt = "Sweet Shirt";
+  static const String kazak = "Kazak";
+  static const String macbook_pro = "Macbook Pro";
+  static const String galatasaray_ceket = "Galatasaray Ceketi";
+  static const String rolex_saat = "Rolex saat";
+  static const String samsung_telefon = "Samsung Telefon";
+}
+
+class ItemPrices {
+  static const int gucci_price = 15250;
+  static const int iphone_price = 10200;
+  static const int sweet_price = 150;
+  static const int kazak_price = 100;
+  static const int macbook_price = 4500;
+  static const int ceket_price = 250;
+  static const int rolex_price = 15500;
+  static const int samsung_price = 3500;
+}
+
+class ImageUrls {
+  static const gucciCanta = "assets/images/gucci_canta.png";
+  static const iphoneTelefon = "assets/images/iphone_15.png";
+  static const sweetShirt = "assets/images/kapsun.png";
+  static const kazak = "assets/images/kazak.png";
+  static const mackbookPro = "assets/images/macbook_pro.png";
+  static const gsCeket = "assets/images/pngegg.png";
+  static const rolexSsaat = "assets/images/rolex_saat.png";
+  static const samsungTelefon = "assets/images/samsung_telefon.png";
+}
+
+enum ItemCategories { Electronics, Fashion, Toys, Books }
+
+enum itemSourceSite { Babazon, xBay, Tabu, Trendway }
+
+enum ItemSuppliers { aliFather, babey, dropper }
